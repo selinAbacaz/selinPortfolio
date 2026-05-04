366e8b2561b5e41e35b38b5cd274ce3cc570f953
Selin A Bacaz <selinb@udel.edu>
2026-05-04 01:54:43 -0400

    added base

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,15 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),
+  
 ],
    resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      
     },
   },
+  base:"/selinPortfolio/"
 })
 
 
